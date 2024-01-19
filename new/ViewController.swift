542d71796cd8fe7701927145b1d99c1447c7b279
김민준 <kmj392766m@gmail.com>
2024-01-19 15:19:55 +0900

    Initial Commit

---
+++ new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by 김민준 on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
